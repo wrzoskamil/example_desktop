9aed702c67d9b9bded6989d5dea86fbde84fdbd6
Kamil Wrzos <kamilwrzos999@gmail.com>
2023-04-17 22:17:10 +0200

    Dodanie zadania 1.6

--- solo-work/solo_1.py
+++ solo-work/solo_1.py
@@ -25,3 +25,13 @@ liczba = 3
 potega = 4
 wynik = liczba**potega
 print("Wynik wynosi: ", wynik)
+
+#zadanie 1.6
+ciag_znakow = "edbw(hdakqas(skqskahb))adwndwb(wgwidn()dsqwhjdw)"
+liczba_nawiasow_otwierajacych = 0
+
+for znak in ciag_znakow:
+    if znak == "(":
+        liczba_nawiasow_otwierajacych += 1
+
+print("Liczba nawiasow otwierajacych wynosi: " + str(liczba_nawiasow_otwierajacych))
