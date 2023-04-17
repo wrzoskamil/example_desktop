6935ee69547f6e21f74e48987f4d8a45b043d465
Kamil Wrzos <kamilwrzos999@gmail.com>
2023-04-17 22:12:45 +0200

    Dodanie zadania 1.2

--- solo-work/solo_1.py
+++ solo-work/solo_1.py
@@ -3,3 +3,8 @@ hello = "Hello"
 student = "Kamil"
 wynik = "{}, {}".format(hello,student)
 print(wynik)
+
+#zadanie 1.2
+student = input("Podaj imię: ")
+komunikat = "Hello, {}!".format(student)
+print(komunikat)
